d90db2ec08530b52004e9789c823ec87514a661a
Julien Hnery <jwh_917@yahoo.com>
2022-05-05 16:46:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,33 +6,26 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 const quotesUrl = "http://localhost:3000/quotes?_embed=likes"
-
 const quoteUl = document.getElementById("quote-list")
 
 function fetchQuotes(){
 
   fetch(quotesUrl)
-  .then(response => response.json())
+  .then(response =>  response.json())
   .then(data => {
   // console.log("data:", data)
-    document.getElementById("quote-list").innerHTML = ""
-
+    quoteUl.innerHTML = ""
     renderQuotes(data)
 
   })
 
 }
 
-function renderQuotes(qouteInfo){
+function renderOneQuote(element){
 
-  qouteInfo.forEach(element => {
-
-    // console.log(element.quote)
-    // console.log(element.likes)
-    // console.log(element.likes.length)
-
-    let quoteLi = document.createElement("li")
-    quoteLi.className = "quote-card"
+  let quoteLi = document.createElement("li")
+    quoteLi.className = `quote-card-${element.id}`
+    quoteLi.id = `quote-${element.id}`
 
     let deleteButton = document.createElement("button")
     deleteButton.id = `deleteButton-${element.id}`
@@ -40,7 +33,7 @@ function renderQuotes(qouteInfo){
     deleteButton.innerHTML = "Delete"
     
     let likeButton = document.createElement("button")
-    likeButton.id = element.id
+    likeButton.id = `likeButton-${element.id}`
     likeButton.className = "btn-success"
     likeButton.innerHTML = "Like: "
     let likeButtonSpan = document.createElement("span")
@@ -49,7 +42,6 @@ function renderQuotes(qouteInfo){
     likeButton.appendChild(likeButtonSpan)
 
     //pass in deleteButtonFunc and likeButtonFunc
-
     deleteButton.addEventListener("click", () => {
 
       deleteButtonFunc(element.id, quoteLi)
@@ -57,7 +49,7 @@ function renderQuotes(qouteInfo){
 
     likeButton.addEventListener("click", () => {
 
-      likeButtonFunc(element.id)
+      likeButtonFunc(element.id, quoteLi)
     })
 
     quoteLi.innerHTML =  
@@ -67,16 +59,24 @@ function renderQuotes(qouteInfo){
       <br>
     </blockquote>
     `
-  
     quoteLi.appendChild(deleteButton)
     quoteLi.appendChild(likeButton)
-  
     quoteUl.appendChild(quoteLi)
+
+}
+
+function renderQuotes(qouteInfo){
+  // console.log(qouteInfo)
+  qouteInfo.forEach(element => {
+    // console.log(element.quote)
+    // console.log(element.likes)
+    // console.log(element.likes.length)
+
+    renderOneQuote(element)
     
   })
 
 }
-
 
 // values of form
 const newFormQuote = document.getElementById("new-quote")
@@ -88,7 +88,7 @@ function formSubmitButtonFunc(){
   formSubmitButton.addEventListener("submit", function(event) {
     
     event.preventDefault()
-    console.log(event)
+    // console.log(event)
 
     fetch(quotesUrl, {
       method: "POST",
@@ -106,24 +106,15 @@ function formSubmitButtonFunc(){
       .then(response => response.json())
       .then(data => {
   
-        console.log('Success:', data)
+        // console.log('Success:', data)
+        renderOneQuote(data)
       
       })
-
-      // fetch(quotesUrl)
-      // .then(response => response.json())
-      // .then(data => {
-
-      //   console.log(data)
-
-      //   // document.getElementById("quote-list").innerHTML = ""
-      // })
-      fetchQuotes()
+   
       formSubmitButton.reset()
   })
 
 }
-
 
 function deleteButtonFunc(quoteId, quoteLi){
   
@@ -139,50 +130,19 @@ function deleteButtonFunc(quoteId, quoteLi){
      
     console.log(data)
 
-
     })   
     
 }
 
-let numHolder = 0
-
-function renderLikes(quoteId){
-  console.log(quoteId)
-  // go thru every span set the innerHTML = ""
-  // 2nd fetch GET - getting the element.likes.length
-  // set all spans innerHTML = likes array length
-  fetch(quotesUrl)
-    .then(response => response.json())
-    .then(data => {
-    // console.log("data:", data)
-  
-    data.forEach(element => {
-      console.log(element)
-      // console.log(element.id)
-      // console.log(element.quote)
-      // console.log(element.likes)
-      // console.log(element.likes.length)
-      if(quoteId === element.id){
-        console.log("GO")
-        numHolder = element.likes.length
-      }
-
-    })
-    console.log(numHolder)
-    document.getElementById(`span-${quoteId}`).innerHTML = numHolder
-
-    })
-  
-  
-  }
-
 const quotesLikesUrl = "http://localhost:3000/likes"
+let time = new Date()
+// console.log(time.getTime())
 
-
-function likeButtonFunc(quoteId){
+function likeButtonFunc(quoteId, quoteLi){
 
   console.log(quoteId)
-  console.log(typeof quoteId)
+  console.log(quoteLi)
+  // console.log(typeof quoteId)
         
   fetch(quotesLikesUrl,{
     method: "POST",
@@ -192,38 +152,16 @@ function likeButtonFunc(quoteId){
       body: JSON.stringify(
            {
            "quoteId": quoteId,
-           "createdAt": 1558524360
-           }   
+           "createdAt": time.getTime()
+           }
       ),
     })
     .then(response => response.json())
-    .then(data => {
-  
-      console.log('Success:', data)
+    .then(data => {  
+      // console.log('Success:', data)
 
-      document.getElementById(`span-${quoteId}`).innerHTML = ""
-
+      document.getElementById(`span-${quoteId}`).innerHTML++
     })
 
-    // fetchQuotes()
-    renderLikes(quoteId)
-
 }   
-      // like fetch
-
-      // update DOM here 
-      // go thru data and get the 
-      // likes array for that quote
-      // innerHTML = likes array length
-      // 0 to 1, 1 to 2
-
-
-// addEventListener for like button
-
-// fetch POST TO APDATE(ADDS TO ARRAY OF LIKES) LIKES WITH A USER`
-
-// UPDATE DOM TO CURRENT LIKES
-
-//LIKES ARRAY LENGTH
-
 
